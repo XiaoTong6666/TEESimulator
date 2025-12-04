e90c5ab18fec5150f2e969cb5c779c7ccb57e11c
XiaoTong6666 <3278671549@qq.com>
2025-12-04 23:48:29 +0800

    Revert changes to Keystore2Interceptor.kt

--- app/src/main/java/org/matrix/TEESimulator/interception/keystore/Keystore2Interceptor.kt
+++ app/src/main/java/org/matrix/TEESimulator/interception/keystore/Keystore2Interceptor.kt
@@ -104,12 +104,7 @@ object Keystore2Interceptor : AbstractKeystoreInterceptor() {
             val keyId = KeyIdentifier(callingUid, descriptor.alias)
 
             if (code == DELETE_KEY_TRANSACTION) {
-                val isSoftwareKey = KeyMintSecurityLevelInterceptor.getGeneratedKeyResponse(keyId) != null
                 KeyMintSecurityLevelInterceptor.cleanupKeyData(keyId)
-                if (isSoftwareKey) {
-                    SystemLogger.info("[TX_ID: $txId] Intercepted deleteKey for Software Key '${descriptor.alias}'. Preventing hardware call.")
-                    return InterceptorUtils.createSuccessReply()
-                }
                 return TransactionResult.ContinueAndSkipPost
             }
 
